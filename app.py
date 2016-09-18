7b3acb96e0a62755664b13cec2be3890b17c44d1
Margot Silberblatt <margotsilberblatt@Margots-MacBook.local>
2016-09-18 08:55:56 -0400

    Modified to include the api.py file

--- app.py
+++ app.py
@@ -7,7 +7,34 @@ import sampleModule
 
 app = Flask(__name__)
 
+# API access
+from urllib.request import urlopen
+import json
 
+AMADAEUS_KEY = 'TmP45v08vUF28rYQ0AxfVOkM93idify9'
+
+def amadeus_search(origin, destination, departure_date, price, airline):
+    api_key = AMADAEUS_KEY
+    url = 'https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey='+ api_key
+    if origin != NULL: #if user does not input a destination
+        url = url + "&origin=" + origin
+    if price != NULL:
+        url = url += "&destination=" + destination
+    #departure date includes return date and is formatted: yyyy-MM-dd--yyyy-MM-dd (departure date--return date)
+    if departure_date != NULL:
+        url = url += "&departure_date=" + departure_date
+    if price != NULL:
+        url = url += "&price=" + price
+    if airline != NULL:
+        url = url += "&airline=" + airline
+    
+    json_obj = urlopen(final_url).read().decode('UTF-8')
+    data = json.loads(json_obj)
+
+    for item in data['results']:
+        print (item['destination'], item['departure_date'], item['return_date'], item['price'], item['airline'])
+
+#Bot takes user input and parses it
 
 @app.route('/', methods=['GET', 'POST'])
 def webh():
@@ -20,7 +47,7 @@ def webhook():
       data = json.loads(request.data.decode("utf-8"))
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-    
+
       payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
 
       r = requests.post('https://graph.facebook.com/v2.7/me/messages/?access_token=' + token, json=payload) # Lets send it
