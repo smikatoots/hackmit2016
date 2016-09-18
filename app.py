517195bb9effbf7bf8fad48c85d5b5c3f4b9af4d
andyvuong <andyv.42@gmail.com>
2016-09-18 08:54:17 -0400

    simple

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 verificationToken = "mozart_verification"
 token = "EAAduIpA2oNMBANEb4HonZBtUlE5CBiZAEd2ZCH152NLM4gN9ynFAVHmcaw6Ckif0YDbEwJUwSWppmVN8bJ4I0BCUmBpDkQG0n6KjIlvxyp6882a7X0L69ZCO6Lb0ZAmifQELQSebcdFZAORCzOZCk8Wbuu6wOB8nqE8zF6J0pg8SgZDZD"
 
+loopIndex = 0
+
 @app.route('/', methods=['GET', 'POST'])
 def webh():
   return "done"
@@ -21,8 +23,16 @@ def webhook():
       data = json.loads(request.data.decode("utf-8"))
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-    
-      payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
+
+      message = ""
+      if loopIndex == 0:
+        message = "Hello Welcome to Mozart Travels! Please enter an origin and destination in the format <origin><destination>! For instance: Chicago,Boston"
+        loopIndex++
+      if loopIndex == 1:
+        message = text
+        loopIndex = 0
+
+      payload = {'recipient': {'id': sender}, 'message': {'text': message}} # We're going to send this back
 
       r = requests.post('https://graph.facebook.com/v2.7/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
